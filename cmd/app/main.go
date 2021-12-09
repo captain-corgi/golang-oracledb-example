b4c7e0440b0d742f93138e247a3ce831258d1558
Anh Le <anh_tuan.l@kbtg.tech>
2021-12-10 00:52:04 +0700

    Add example for query a table in db

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,7 +1,77 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/captain-corgi/golang-oracledb-example/pkg/oracle"
+)
 
 func main() {
+	// Greeting
 	fmt.Println("Hello, Oracle DB!")
+
+	// Create connection
+	conn := oracle.NewConnection()
+	defer conn.Close()
+
+	// Create statement
+	stmt, err := conn.Prepare(`SELECT
+									EMPLOYEE_ID,
+									FIRST_NAME,
+									LAST_NAME,
+									EMAIL,
+									PHONE,
+									HIRE_DATE,
+									MANAGER_ID,
+									JOB_TITLE
+								FROM
+									EMPLOYEES`)
+	if err != nil {
+		panic(err)
+	}
+	defer stmt.Close()
+
+	// Execute statement
+	rows, err := stmt.Query()
+	if err != nil {
+		panic(err)
+	}
+
+	// Iterate over results
+	for rows.Next() {
+		// Define columns
+		var EMPLOYEE_ID,
+			FIRST_NAME,
+			LAST_NAME,
+			EMAIL,
+			PHONE,
+			HIRE_DATE,
+			MANAGER_ID,
+			JOB_TITLE sql.NullString
+
+		// Scan columns
+		err = rows.Scan(&EMPLOYEE_ID,
+			&FIRST_NAME,
+			&LAST_NAME,
+			&EMAIL,
+			&PHONE,
+			&HIRE_DATE,
+			&MANAGER_ID,
+			&JOB_TITLE)
+		if err != nil {
+			panic(err)
+		}
+
+		// Print results
+		fmt.Println(EMPLOYEE_ID.String,
+			FIRST_NAME.String,
+			LAST_NAME.String,
+			EMAIL.String,
+			PHONE.String,
+			HIRE_DATE.String,
+			MANAGER_ID.String,
+			JOB_TITLE.String)
+	}
+
 }
--- pkg/oracle/oracle.go
+++ pkg/oracle/oracle.go
@@ -6,10 +6,11 @@ import (
 	_ "github.com/sijms/go-ora/v2"
 )
 
-func NewConnection() {
-	db, err := sql.Open("oracle", "oracle://OT:yourpassword@localhost/ORCLPDB1.localdomain")
+func NewConnection() *sql.DB {
+	conn, err := sql.Open("oracle", "oracle://OT:yourpassword@localhost/ORCLPDB1.localdomain")
 	if err != nil {
 		panic(err)
 	}
-	defer db.Close()
+
+	return conn
 }
